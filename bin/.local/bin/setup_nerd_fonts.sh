ae2cc7e3ee02e6fd04b6730dc98cceb791c8c754
Augusto Altenhofen <augustoaltenhofen@protonmail.com>
2025-12-19 18:06:48 -0300

    refactor bin

--- bin/.local/bin/setup_nerd_fonts.sh
+++
@@ -1,9 +0,0 @@
-FONT=CascadiaCode
-
-curl -o /tmp/$FONT.zip -L https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/$FONT.zip
-
-unzip /tmp/$FONT.zip -d ~/.local/share/fonts/
-
-fc-cache -vf ~/.local/share/fonts/
-
-echo "installed $FONT font"
